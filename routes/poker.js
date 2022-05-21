c43fd2e30b04b94aa4678e4403d8e7a3e4b549f6
Ben Davis <ben@bgjd.io>
2022-05-21 16:43:04 +0100

    Tidy up dead code

--- routes/poker.js
+++ routes/poker.js
@@ -1,15 +1,12 @@
 var express = require('express');
-var path = require('path')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req,res,next) {
-  //res.sendFile(path.join(__dirname, 'join.html'));
   res.render('join');
 })
 
 router.get('/:room', function(req, res, next) {
-  //res.sendFile(path.join(__dirname,'poker.html'));
   res.render('poker');
 });
 
